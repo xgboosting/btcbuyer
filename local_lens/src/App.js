525a452c23cf5d5be3d780ac5937815f92e54900
llennox <gonnellcough@gmail.com>
2018-04-29 16:07:32 -0700

    logos

--- local_lens/src/App.js
+++ local_lens/src/App.js
@@ -4,6 +4,10 @@ import Home from './components/Home';
 
 class App extends Component {
 
+componentDidMount() {
+  document.title = "bitagora";
+}
+
   render() {
     return (
     <div style={{ height: '100%'}}>
