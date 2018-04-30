e3b890f8eca6509ee464b3645b90f97dbbcb2d03
llennox <gonnellcough@gmail.com>
2018-04-29 17:32:59 -0700

    percent

--- cryptoapp/views.py
+++ cryptoapp/views.py
@@ -532,7 +532,7 @@ class Misc(APIView):
             print(type(order.price))
             print(order.price)
             percent = float(order.price) * 0.031
-            amount = percent + order.price
+            amount = percent + float(order.price)
             payload = {'name': order_uuid,'description': order.url, 'local_price': {'amount':amount,'currency':'USD'},'pricing_type': 'fixed_price','metadata': {'customer_id': str(order.user_uuid) }}
             response = requests.post(url, headers=headers, json=payload)
             data = response.json()
